e8d75da683218bd8dac206403d00f9a82c17411f
Unknown <lutfi@dr.com>
2019-07-12 05:51:04 +0700

    Fix administrator

--- application/views/hak_akses_user/akses_adr.php
+++ application/views/hak_akses_user/akses_adr.php
@@ -8,35 +8,6 @@
 		<a href="<?= base_url('site/bagian') ?>" class="menu-item">Bagian</a>
 	</div>
 </div>
-<div class="nav-item has-sub">
-	<a href="#"><i class="ik ik-users"></i><span>Customer</span></a>
-	<div class="submenu-content">
-		<a href="<?= base_url('site/tipe_customer') ?>" class="menu-item">Tipe Customer</a>
-		<a href="<?= base_url('site/data_customer') ?>" class="menu-item">Data Customer</a>
-	</div>
-</div>
-
-<div class="nav-item has-sub">
-	<a href="#"><i class="ik ik-link"></i><span>Vendor</span></a>
-	<div class="submenu-content">
-		<a href="<?= base_url('site/data_vendor') ?>" class="menu-item">Data Vendor</a>
-	</div>
-</div>
-
-<div class="nav-item has-sub">
-	<a href="#"><i class="ik ik-edit"></i><span>Project</span></a>
-	<div class="submenu-content">
-		<a href="<?= base_url('site/data_project') ?>" class="menu-item">Data Project</a>
-	</div>
-</div>
-
-<div class="nav-item has-sub">
-	<a href="#"><i class="ik ik-check-square"></i><span>Sales</span></a>
-	<div class="submenu-content">
-		<a href="<?= base_url('site/data_sales') ?>" class="menu-item">Data Sales</a>
-		<a href="<?= base_url('site/jabatan') ?>" class="menu-item">Jabatan</a>
-	</div>
-</div>
 
 <div class="nav-lavel">ADMIN MASTER</div>
 <div class="nav-item">
