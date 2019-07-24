882c8e4d9065a17430a69794348f84107ba3f633
Idham <lutfi@dr.com>
2019-07-24 16:02:51 +0700

    fix arus dana tidak bisa tmpil

--- application/models/Arusdana_model.php
+++ application/models/Arusdana_model.php
@@ -16,8 +16,8 @@ class Arusdana_model extends CI_Model {
 			select 'arus_dana' as jenis, id_arus_dana as id, tanggal, no_arus_dana as nomor, uk.nama_unit_kerja, kt.nama_kategori,an.kode_anggaran, an.nama_anggaran, periode_pelaksanaan, 'W' as status_realisasi
 			from arus_dana a
 			left join unit_kerja uk on a.id_unit_kerja = uk.id_unit_kerja
-			join anggaran an on a.id_anggaran = an.id_anggaran
-			join kategori kt on a.id_kategori = kt.id_kategori
+			left join anggaran an on a.id_anggaran = an.id_anggaran
+			left join kategori kt on a.id_kategori = kt.id_kategori
 			where a.id_bagian = ? and id_permintaan is null
 			UNION
 			select 'permintaan' as jenis, id_permintaan as id, tanggal, no_anggaran as nomor,  uk.nama_unit_kerja, kt.nama_kategori,an.kode_anggaran, an.nama_anggaran, NULL as periode_pelaksanaan, status_realisasi
